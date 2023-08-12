e0f42eca554d8302d264502069eeb269a9d77f94
SafeAreaView <safeareaview@gmail.com>
2023-08-12 12:27:34 +0300

    Fix: fix code succession

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles/index.scss";
 
+// burger
+
 document.addEventListener("DOMContentLoaded", function () {
   const burgerIcon = document.querySelector(".burger-icon");
   const burgerNav = document.querySelector(".burger-nav");
@@ -14,3 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//Modal
+
+function openModal() {
+  var modalOverlay = document.getElementById('modalOverlay');
+  var modalContent = document.getElementById('modalContent');
+  modalOverlay.style.display = 'flex';
+  modalContent.innerHTML = '<p>Здесь можно добавить свой контент для модального окна</p>';
+}
+
+function closeModal() {
+  var modalOverlay = document.getElementById('modalOverlay');
+  modalOverlay.style.display = 'none';
+}
